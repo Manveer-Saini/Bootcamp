4de779875b4bc4ea62896feda565e32566e29a99
Manveer-Saini <zolbly@gmail.com>
2021-05-18 00:10:16 -0700

    added loops-algo.js

---
+++ CodingDojo/WebFundamentals/JavaScript/Assignments/loops-algo.js
@@ -0,0 +1,14 @@
+// Algorithm 1:
+
+for(let i = 1; i < 20; i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+// Algorithm 2: 
+let sum = 0;
+for(let i = 1; i <= 5; i++){
+    sum += i;
+    console.log("Num: ", i + ",", "Sum: ", sum);
+}
