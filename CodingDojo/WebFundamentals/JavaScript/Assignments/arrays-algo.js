a1d9493cc6f92edfa8d7b71d99987989aee1932e
Manveer-Saini <zolbly@gmail.com>
2021-05-18 23:00:18 -0700

    added arrays-algo.js

---
+++ CodingDojo/WebFundamentals/JavaScript/Assignments/arrays-algo.js
@@ -0,0 +1,11 @@
+let testArr = [6,3,5,1,2,4]
+let sum = 0;
+for(let i = 0; i < testArr.length; i++){
+    sum += testArr[i];
+    console.log("Num:", testArr[i] + ",", "Sum:", sum);
+}
+
+for(let i = 0; i < testArr.length; i++){
+    testArr[i] *= i;
+}
+console.log(testArr);
