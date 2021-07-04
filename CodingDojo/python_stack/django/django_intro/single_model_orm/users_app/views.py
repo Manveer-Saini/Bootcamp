708e0e5c402d610d23ca06d91f4f0c7c6d3c365b
Manveer-Saini <zolbly@gmail.com>
2021-07-03 23:05:31 -0700

    added new projects

--- CodingDojo/python_stack/django/django_intro/single_model_orm/users_app/views.py
+++ CodingDojo/python_stack/django/django_intro/single_model_orm/users_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Users
 
+
+def create(request):
+    someUser = Users.objects.create(first_name=request.POST["first_name"], last_name=request.POST["last_name"],
+                                    email_address=request.POST["email_address"], age=request.POST["age"])
+
+    return redirect("/results")
 # Create your views here.
+
+
+def display(request):
+    context = {
+        "all_users" : Users.objects.all()
+    }
+    return render(request, "results.html", context)
