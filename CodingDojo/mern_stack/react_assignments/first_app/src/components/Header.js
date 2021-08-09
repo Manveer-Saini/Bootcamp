a0c05010a4c5cc5195755a5abca4a3d9627caf07
Manveer Saini <manveersaini@pop-os.localdomain>
2021-08-08 21:41:58 -0700

    added Todo List and Header

---
+++ CodingDojo/mern_stack/react_assignments/first_app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Header = () => {
+    return(
+        <div>
+            <h1>Hello Dojo!</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ CodingDojo/mern_stack/react_assignments/first_app/src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react';
+
+const TodoList = () => {
+    return (
+        <div>
+            <h3>Things I need to do:</h3>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
