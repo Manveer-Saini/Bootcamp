7ee4b574319457a04ca5a577fffa8d008fbb3d68
Manveer Saini <manveersaini@pop-os.localdomain>
2021-08-10 22:42:01 -0700

    changed indentation on App.js

--- CodingDojo/mern_stack/react_assignments/prop_it_up/src/App.js
+++ CodingDojo/mern_stack/react_assignments/prop_it_up/src/App.js
@@ -4,10 +4,33 @@ import PersonCard from './components/PersonCard'
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName = {"John"} lastName = {"Doe"} age = {34} hairColor = {"Blue"} />
-      <PersonCard firstName = {"Jenny"} lastName = {"Austin"} age = {23} hairColor = {"Black"} />
-      <PersonCard firstName = {"Michael"} lastName = {"Scott"} age = {44} hairColor = {"Red"} />
-      <PersonCard firstName = {"Harry"} lastName = {"Thomas"} age = {14} hairColor = {"Green"} />
+      <PersonCard
+        firstName={"John"}
+        lastName={"Doe"}
+        age={34}
+        hairColor={"Blue"}
+      />
+
+      <PersonCard
+        firstName={"Jenny"}
+        lastName={"Austin"}
+        age={23}
+        hairColor={"Black"}
+      />
+
+      <PersonCard
+        firstName={"Michael"}
+        lastName={"Scott"}
+        age={44}
+        hairColor={"Red"}
+      />
+
+      <PersonCard
+        firstName={"Harry"}
+        lastName={"Thomas"}
+        age={14}
+        hairColor={"Green"}
+      />
     </div>
   );
 }
