01b3416de019d3589dfc01bcef7a61941ec11896
Manveer Saini <manveersaini@pop-os.localdomain>
2021-08-13 01:01:34 -0700

    added new age increase button

--- CodingDojo/mern_stack/react_assignments/prop_it_up/src/components/PersonCard.js
+++ CodingDojo/mern_stack/react_assignments/prop_it_up/src/components/PersonCard.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
     const { firstName, lastName, age, hairColor } = props;
+    const [theAge, setAge] = useState(age);
+
+    const increaseAge = () => {
+        setAge(theAge + 1);
+    }
 
     return (
         <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {theAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={increaseAge}>Birthday button for {firstName} {lastName}</button>
         </div>
 
     )
