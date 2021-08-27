93dee91f8d14e3e16ff955e3a077729d6694b3c2
Manveer-Saini <zolbly@gmail.com>
2021-08-26 21:39:26 -0700

    Added pokemon api to display 807 pokemon.

---
+++ CodingDojo/mern_stack/react_assignments/pokemon_list/src/components/Display.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
+
+export default function Display(props){
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+        .then(response => response.json())
+        .then(response => setPokemon(response.results))
+    }, []);
+
+    return(
+        <div>
+            <ul>
+                {pokemon.length > 0 && pokemon.map((thePokemon, index) => {
+                    return(<li>{thePokemon.name}</li>)
+                })}
+            </ul>
+            
+        </div>
+    );
+}
+
